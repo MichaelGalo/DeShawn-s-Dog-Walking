4c39000d2c8818bb78f66657026a13b6ad3a2d7c
Michael Galo <michael@michaelthinks.co>
2024-06-06 15:29:41 -0500

    added walkers state

--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -6,7 +6,7 @@ export const Walkers = () => {
   let walkerHTML = "<ul>";
 
   for (const walker of walkers) {
-    walkerHTML += `<li>${walker.name}</li>`; // 2 fix
+    walkerHTML += `<li data-id="${walker.id}" data-city="${walker.city}">${walker.name}</li>`; // 2 fix
   }
 
   walkerHTML += "</ul>";
